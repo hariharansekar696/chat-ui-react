d4a30ed2381c0255cedebc5eab67f1a235ab20f8
hariharan <hariharansekar696@gmail.com>
2021-07-18 18:21:01 +0530

    worked on side bar and chat list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { CHATS } from "./utils/constants";
 
 function App() {
+  function handleThemeChange(themeColor) {
+    for (let i = 0; i < document.body.classList.length; i++) {
+      const className = document.body.classList[i];
+
+      if (className.startsWith("theme-")) {
+        document.body.classList.remove(className);
+      }
+    }
+    document.body.classList.add(themeColor || "theme-purple");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <div className="row chat-container">
+          <div className="col-lg-4 chat-left-layout">
+            <div class="row h-100">
+              {/* Side bar starts here */}
+              <div class="sidebar-left col-lg-2">
+                <div class="sidebar-left-inner">
+                  <ul>
+                    <li>
+                      <span class="input-group-text">
+                        <i class="far fa-comment-alt"></i>
+                      </span>
+                    </li>
+                    <li>
+                      <span class="input-group-text active">
+                        <i class="fas fa-user-friends"></i>
+                      </span>
+                    </li>
+                    <li>
+                      <span class="input-group-text">
+                        <i class="fas fa-cog"></i>
+                      </span>
+                    </li>
+                    <li class="theme">
+                      <div
+                        onClick={() => handleThemeChange("theme-default")}
+                      ></div>
+                      <div
+                        onClick={() => handleThemeChange("theme-dark")}
+                      ></div>
+                      <div
+                        onClick={() => handleThemeChange("theme-purple")}
+                      ></div>
+                    </li>
+                    <li>
+                      <img
+                        src="http://chatvia-light.react.themesbrand.com/static/media/avatar-3.2cfd5ba6.jpg"
+                        class="rounded-circle avatar"
+                        alt="chat-ui"
+                      />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              {/* side bar ends here */}
+
+              {/* chat starts here */}
+              <div class="chat-users col-lg-10">
+                <ul class="list-group-header">
+                  <li class="list-group-header-title">
+                    <i class="fas fa-chevron-left list-group-header-icon"></i>
+                    Chats
+                  </li>
+                  <li>
+                    <div class="input-group">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Search Messages or users"
+                      />
+                      <span class="input-group-text">
+                        <i class="fas fa-search"></i>
+                      </span>
+                    </div>
+                  </li>
+                </ul>
+                <ul class="list-group-chat">
+                  {CHATS.map((chat) => (
+                    <li
+                      className={
+                        chat.isActive ? "list-item active" : "list-item"
+                      }
+                    >
+                      <div class="list-item-inner">
+                        <div
+                          className={
+                            chat.isOnline
+                              ? "chat-user-img online"
+                              : "chat-user-img"
+                          }
+                        >
+                          <img
+                            src={chat.profilePic}
+                            class="rounded-circle avatar"
+                            alt="chat-ui"
+                          />
+                          <span class="user-status"></span>
+                        </div>
+                        <div class="chat-user">
+                          <h5 class="chat-user-name text-truncate">
+                            {chat.name}
+                          </h5>
+                          <p class="chat-user-message">
+                            {chat?.lastMessage?.message}
+                          </p>
+                        </div>
+                        <div class="chat-user-time">
+                          {chat?.lastMessage?.timeStamp}
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-8 chat-right-layout">right layout</div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,290 @@
+export const CHATS = [
+  {
+    _id: "60f160b23c459d36a1b149dd",
+    name: "Patrick Hendricks",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+
+    isTyping: true,
+    unSeenCount: 5,
+    lastMessage: {
+      message: "okay sure😄👍",
+      timeStamp: "11:47 AM"
+    },
+    isActive: true
+  },
+  {
+    _id: "60f160b25cccef83298269b3",
+    name: "Lara",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1614289371518-722f2615943d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: false,
+    unSeenCount: 7,
+    lastMessage: {
+      message: "Image",
+      timeStamp: "04:23 PM"
+    }
+  },
+  {
+    _id: "60f160b277656729712343b6",
+    name: "Poole",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 10,
+    lastMessage: {
+      message: "Okay 👍, let me check it and get back to you soon",
+      timeStamp: "08:20 PM"
+    }
+  },
+  {
+    _id: "60f160b20a056b11cc9193e2",
+    name: "Marilyn",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1584999734482-0361aecad844?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 4,
+    lastMessage: {
+      message: "minim",
+      timeStamp: "07:19 PM"
+    }
+  },
+  {
+    _id: "60f160b2404b06e3dd858f68",
+    name: "Roberts",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1546422401-68b415cbf8de?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 10,
+    lastMessage: {
+      message: "sint",
+      timeStamp: "03:32 PM"
+    }
+  },
+  {
+    _id: "60f160b2a4acb158a79df190",
+    name: "Wallace",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1602471615287-d733c59b79c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 5,
+    lastMessage: {
+      message: "tempor",
+      timeStamp: "08:13 PM"
+    }
+  },
+  {
+    _id: "60f160b2dd5d35874896ad0e",
+    name: "Mayer",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1532074205216-d0e1f4b87368?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 9,
+    lastMessage: {
+      message: "minim",
+      timeStamp: "03:10 PM"
+    }
+  },
+  {
+    _id: "60f197b2802714e188b3f182",
+    name: "Anthony",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzR8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: false,
+    unSeenCount: 2,
+    lastMessage: {
+      message: "sunt",
+      timeStamp: "07:02"
+    }
+  },
+  {
+    _id: "60f197b2e9da3913bfb0ac2f",
+    name: "Burnett",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1504826260979-242151ee45b7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: false,
+    unSeenCount: 1,
+    lastMessage: {
+      message: "ad",
+      timeStamp: "03:41"
+    }
+  },
+  {
+    _id: "60f197b2463aa7f1708d905e",
+    name: "Owens",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1621550418658-5539addecea8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njl8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 8,
+    lastMessage: {
+      message: "eiusmod",
+      timeStamp: "09:10"
+    }
+  },
+  {
+    _id: "60f197b29555a2b6036cb546",
+    name: "Nelda",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1506697084665-7f7d652308af?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: false,
+    unSeenCount: 3,
+    lastMessage: {
+      message: "dolor",
+      timeStamp: "08:51"
+    }
+  },
+  {
+    _id: "60f197b2273b7ac9ed204623",
+    name: "Cherry",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1534308143481-c55f00be8bd7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 7,
+    lastMessage: {
+      message: "non",
+      timeStamp: "06:25"
+    }
+  },
+  {
+    _id: "60f197b2a135e83180da8318",
+    name: "Rodriquez",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1522307837370-cc113a36b784?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 10,
+    lastMessage: {
+      message: "mollit",
+      timeStamp: "12:14"
+    }
+  },
+  {
+    _id: "60f197b2118e865fb31eec1f",
+    name: "Casandra",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1608493573324-b055427a503e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 9,
+    lastMessage: {
+      message: "magna",
+      timeStamp: "01:47"
+    }
+  },
+  {
+    _id: "60f197b2626caea02f988f94",
+    name: "Barber",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1525550557089-27c1bfedd06c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: false,
+    unSeenCount: 2,
+    lastMessage: {
+      message: "aliqua",
+      timeStamp: "11:01"
+    }
+  },
+  {
+    _id: "60f197b264663909b4723702",
+    name: "Irma",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1554780336-390462301acf?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 6,
+    lastMessage: {
+      message: "qui",
+      timeStamp: "05:23"
+    }
+  },
+  {
+    _id: "60f197b20e226322d4cfaae4",
+    name: "Patrick",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1567303314286-6735a4ad9d42?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 5,
+    lastMessage: {
+      message: "id",
+      timeStamp: "01:36"
+    }
+  },
+  {
+    _id: "60f197b2663bb370b3da0121",
+    name: "Mills",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1620000617482-821324eb9a14?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: true,
+    unSeenCount: 6,
+    lastMessage: {
+      message: "consectetur",
+      timeStamp: "11:48"
+    }
+  },
+  {
+    _id: "60f197b2e6a80898ab3a0dfa",
+    name: "Charlotte",
+    isOnline: false,
+    profilePic:
+      "https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: false,
+    unSeenCount: 2,
+    lastMessage: {
+      message: "sint",
+      timeStamp: "12:35"
+    }
+  },
+  {
+    _id: "60f197b2e8e20ef4f1b93b49",
+    name: "Ursula",
+    isOnline: true,
+    profilePic:
+      "https://images.unsplash.com/photo-1603665696936-1331903666e5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    isTyping: false,
+    unSeenCount: 3,
+    lastMessage: {
+      message: "elit",
+      timeStamp: "05:54"
+    }
+  }
+];
+export const FILES = [
+  {
+    name: "Documents",
+    noOfFiles: "250",
+    size: "180MB",
+    fileType: "documents"
+  },
+  {
+    name: "Photos",
+    noOfFiles: "158",
+    size: "250MB",
+    fileType: "photos"
+  },
+  {
+    name: "Movies",
+    noOfFiles: "980",
+    size: "650MB",
+    fileType: "movies"
+  },
+  {
+    name: "Other",
+    noOfFiles: "255",
+    size: "25MB",
+    fileType: "others"
+  }
+];
